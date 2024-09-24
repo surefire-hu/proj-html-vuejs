894ee0de09da5d15dd77cb75786f1639d4744df2
surefire-hu <surefire.hu2001@gmail.com>
2024-09-24 19:35:11 +0200

    aggiunto AppFooter

--- src/components/footer/AppFooter.vue
+++ src/components/footer/AppFooter.vue
@@ -1,15 +1,18 @@
 <script>
 import AppFooterTop from './AppFooterTop.vue'
+import AppFooterBottom from './AppFooterBottom.vue'
 export default {
     name: 'AppFooter',
     components: {
         AppFooterTop,
+        AppFooterBottom,
     }
 };
 </script>
 
 <template>
     <AppFooterTop/>
+    <AppFooterBottom/>
 </template>
 
 <style>
---
+++ src/components/footer/AppFooterBottom.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name:'AppFooterBottom'
+}
+</script>
+
+<template>
+    <div class="h-16 bg-button flex items-center">
+        <div class="w-2/3 flex justify-between mx-auto text-gray-400">
+            <p>© Copyright GoodLayers, All Right Reserved</p>
+            <div>
+                <a href="#" class="cursor-pointer hover:text-white"> About Us </a>| 
+                <a href="#" class="cursor-pointer hover:text-white"> Accommodation </a>|
+                <a href="#" class="cursor-pointer hover:text-white"> Call For Paper </a>|
+                <a href="#" class="cursor-pointer hover:text-white"> Contact Us</a>
+            </div>
+        </div>
+    </div>
+</template>
